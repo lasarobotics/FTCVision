fd9f3543d7c905af67b2c5b3ffbcd452748b6196
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-10-05 22:12:35 -0500

    Fix FPS counter

--- ftc-visionlib-java/src/main/java/org/lasarobotics/vision/FPS.java
+++ ftc-visionlib-java/src/main/java/org/lasarobotics/vision/FPS.java
@@ -1,6 +1,6 @@
 package org.lasarobotics.vision;
 
-import org.opencv.core.Core;
+import org.lasarobotics.vision.util.RollingAverage;
 
 import java.text.DecimalFormat;
 
@@ -8,51 +8,51 @@ import java.text.DecimalFormat;
  * Allows counting and retrieving a frames per second count.
  */
 public class FPS {
-    private static final int FRAMES_BETWEEN_UPDATE = 20;
+    private static final int FRAME_BUFFER_LENGTH = 5;
     private static final DecimalFormat FPS_FORMAT = new DecimalFormat("0.00");
 
-    private int mFramesCounter;
-    private double mFrequency;
-    private long mprevFrameTime;
-    private double mFps;
+    private RollingAverage<Long> rollingAverage;
+    private long lastTime;
 
     public FPS()
     {
-        mFramesCounter = 0;
-        mFrequency = Core.getTickFrequency();
-        mprevFrameTime = Core.getTickCount();
-
-        mFps = 0;
+        rollingAverage = new RollingAverage<>(FRAME_BUFFER_LENGTH);
+        lastTime = -1L;
     }
 
     /**
      * Update the FPS counter.
      *
      * Call this method EVERY FRAME!
-     * @return The FPS, as a double in frames/second.
      */
     public void update() {
-
-        mFramesCounter++;
-        if (mFramesCounter % FRAMES_BETWEEN_UPDATE == 0) {
-            long time = Core.getTickCount();
-            mFps = FRAMES_BETWEEN_UPDATE * mFrequency / (time - mprevFrameTime);
-            mprevFrameTime = time;
+        if (lastTime != -1L)
+        {
+            long delta = System.nanoTime() - lastTime;
+            rollingAverage.addValue(delta);
         }
+        lastTime = System.nanoTime();
     }
 
     /**
-     * Get the frames per second count.
+     * Get the frames per second count, as a double (prefer using getFPSString() instead).
      * @return The FPS, as a double in frames/second.
      */
     public double getFPS()
     {
-        return mFps;
+        double period = rollingAverage.getAverage() / 1000000000.0; //period: s
+        return 1.0 / period; //frequency = 1/s
     }
 
     public String getFPSString()
     {
-        return FPS_FORMAT.format(mFps);
+        return FPS_FORMAT.format(getFPS());
+    }
+
+    public void pause()
+    {
+        //make sure we resample the FPS on next update()
+        lastTime = -1L;
     }
 
     /**
@@ -60,6 +60,7 @@ public class FPS {
      */
     public void reset()
     {
-        mFramesCounter = 0;
+        rollingAverage.clear();
+        lastTime = -1L;
     }
 }
