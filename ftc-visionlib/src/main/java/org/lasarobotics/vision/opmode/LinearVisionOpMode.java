205da2abddacdf7ba7650d9ce0b0f0e093ee7a8e
Russell Coleman <russellsayshi@gmail.com>
2015-11-23 13:57:10 -0600

    Fixed grammatical stuff

--- ftc-visionlib/src/main/java/org/lasarobotics/vision/opmode/LinearVisionOpMode.java
+++ ftc-visionlib/src/main/java/org/lasarobotics/vision/opmode/LinearVisionOpMode.java
@@ -5,7 +5,7 @@ package org.lasarobotics.vision.opmode;
  */
 public class LinearVisionOpMode {
     public LinearVisionOpMode() {
-        throw new RuntimeException("Stop. Stop it use the right class. Use VisionOpMode." +
+        throw new RuntimeException("Stop. Stop it. Use the right class. Use VisionOpMode." +
                 "Talk to Russell or Arthur if you need help." +
                 "And re-evaluate your life decisions. Non-linear Opmodes make everything better." +
                 "Thank you.");
