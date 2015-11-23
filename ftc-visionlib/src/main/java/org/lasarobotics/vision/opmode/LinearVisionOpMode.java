32855b57aa5385d151b8e82c1ffb8f6db20386b5
Russell Coleman <russellsayshi@gmail.com>
2015-11-23 13:56:37 -0600

    Added new vision opmode meant to be extended named LinearVisionOpMode

---
+++ ftc-visionlib/src/main/java/org/lasarobotics/vision/opmode/LinearVisionOpMode.java
@@ -0,0 +1,13 @@
+package org.lasarobotics.vision.opmode;
+
+/**
+ * Created by S2061383 on 11/23/2015.
+ */
+public class LinearVisionOpMode {
+    public LinearVisionOpMode() {
+        throw new RuntimeException("Stop. Stop it use the right class. Use VisionOpMode." +
+                "Talk to Russell or Arthur if you need help." +
+                "And re-evaluate your life decisions. Non-linear Opmodes make everything better." +
+                "Thank you.");
+    }
+}
