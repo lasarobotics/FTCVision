3c8284bfde809a1c110fe142c0cf309cf1dad512
Russell Coleman <russellsayshi@gmail.com>
2015-11-24 16:14:47 -0600

    Added more comments

--- ftc-visionlib/src/main/java/org/lasarobotics/vision/opmode/TestVisionOpMode.java
+++ ftc-visionlib/src/main/java/org/lasarobotics/vision/opmode/TestVisionOpMode.java
@@ -9,8 +9,8 @@ import org.lasarobotics.vision.opmode.extensions.BeaconColorExtension;
  */
 public class TestVisionOpMode extends VisionOpMode {
     BeaconColorExtension bce = new BeaconColorExtension();
-    private int beaconWaitCounter = 0;
-    private int lastBeaconCenterX = 0;
+    private int beaconWaitCounter = 0; //Do not change, used to count frames while waiting
+    private int lastBeaconCenterX = 0; //Center of beacon on screen last frame
     private int absoluteCenter; //Image width divided by two
     private int moveThreshold; //Allowed deviation of beacon from image center
     private static final int BEACON_WAIT_TIME = 5; //Time to wait before rotation if unable to find beacon
@@ -19,6 +19,7 @@ public class TestVisionOpMode extends VisionOpMode {
     private static final double UNCERTAIN_MOTOR_POWER = 0; //Motor power when unsure what to do
     DcMotor leftBack, rightBack, leftFront, rightFront; //Motors
 
+    //Talk to robot and acquire motors, as well as initiating beacon color extension, and call super's init
     @Override
     public void init() {
         super.init();
@@ -29,10 +30,13 @@ public class TestVisionOpMode extends VisionOpMode {
         rightFront = hardwareMap.dcMotor.get("rightFront");
     }
 
+    //Call super loop, loop beacon color and perform operations
     @Override
     public void loop() {
         super.loop();
         bce.loop(this);
+        
+        //Calculate variables, does this in loop because not set at beginning
         absoluteCenter = height/2;
         moveThreshold = height/19;
 
@@ -41,14 +45,16 @@ public class TestVisionOpMode extends VisionOpMode {
             if(!withinThreshold(beaconCenterX, lastBeaconCenterX, moveThreshold)) {
                 //Last known location of beacon is drastically different from current location
                 if(beaconWaitCounter++ < BEACON_WAIT_TIME) {
-                    unsureMotors();
+                    unsureMotors(); //Stop robot
                     return;
                 }
             }
+            //Go!
             shouldMove(beaconCenterX);
             beaconWaitCounter = 0;
             lastBeaconCenterX = beaconCenterX;
         } else {
+            //Turn around until we can see the beacon
             findBeacon(beaconCenterX);
         }
     }
@@ -82,6 +88,7 @@ public class TestVisionOpMode extends VisionOpMode {
         leftBack.setPower(DRIVE_MOTOR_POWER);
     }
 
+    //Function to call when unsure what to do, presently stops robot.
     public void unsureMotors() {
         rightFront.setPower(UNCERTAIN_MOTOR_POWER);
         rightBack.setPower(UNCERTAIN_MOTOR_POWER);
@@ -89,6 +96,7 @@ public class TestVisionOpMode extends VisionOpMode {
         leftBack.setPower(UNCERTAIN_MOTOR_POWER);
     }
 
+    //Function called when we know robot should move, but not yet where
     public void shouldMove(int beaconCenterX) {
         if(withinThreshold(beaconCenterX, absoluteCenter, moveThreshold)) {
             driveForward();
@@ -101,6 +109,7 @@ public class TestVisionOpMode extends VisionOpMode {
         }
     }
 
+    //Utility method to check if a number is within a threshold
     public static boolean withinThreshold(int item1, int item2, int threshold) { //TODO move to helper method in some other class
         int distanceFrom = Math.abs(item1 - item2);
         return distanceFrom <= threshold;
