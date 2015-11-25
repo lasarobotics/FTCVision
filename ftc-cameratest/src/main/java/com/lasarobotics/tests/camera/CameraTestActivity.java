358a74562dcaca114fad59f306ae1cf5c7857247
Arthur Pachachura <pachachura.arthur@gmail.com>
2015-11-24 21:24:40 -0600

    Code cleanup

--- ftc-cameratest/src/main/java/com/lasarobotics/tests/camera/CameraTestActivity.java
+++ ftc-cameratest/src/main/java/com/lasarobotics/tests/camera/CameraTestActivity.java
@@ -4,16 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import org.lasarobotics.vision.android.Camera;
-import org.lasarobotics.vision.android.Cameras;
-import org.lasarobotics.vision.detection.ColorBlobDetector;
-import org.lasarobotics.vision.detection.objects.Contour;
-import org.lasarobotics.vision.ftc.resq.Beacon;
-import org.lasarobotics.vision.image.Drawing;
-import org.lasarobotics.vision.util.FPS;
-import org.lasarobotics.vision.util.color.ColorGRAY;
-import org.lasarobotics.vision.util.color.ColorHSV;
-import org.lasarobotics.vision.util.color.ColorRGBA;
+import org.lasarobotics.vision.test.android.Camera;
+import org.lasarobotics.vision.test.android.Cameras;
+import org.lasarobotics.vision.test.detection.ColorBlobDetector;
+import org.lasarobotics.vision.test.detection.objects.Contour;
+import org.lasarobotics.vision.test.ftc.resq.Beacon;
+import org.lasarobotics.vision.test.image.Drawing;
+import org.lasarobotics.vision.test.util.FPS;
+import org.lasarobotics.vision.test.util.color.ColorGRAY;
+import org.lasarobotics.vision.test.util.color.ColorHSV;
+import org.lasarobotics.vision.test.util.color.ColorRGBA;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
@@ -24,26 +24,48 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 public class CameraTestActivity extends Activity implements CvCameraViewListener2 {
 
+    private static final ColorHSV colorRadius = new ColorHSV(50, 75, 127);
+    private static final ColorHSV lowerBoundRed = new ColorHSV((int) (305 / 360.0 * 255.0), (int) (0.200 * 255.0), (int) (0.300 * 255.0));
+    private static final ColorHSV upperBoundRed = new ColorHSV((int) ((360.0 + 5.0) / 360.0 * 255.0), 255, 255);
+    private static final ColorHSV lowerBoundBlue = new ColorHSV((int) (170.0 / 360.0 * 255.0), (int) (0.200 * 255.0), (int) (0.750 * 255.0));
+    private static final ColorHSV upperBoundBlue = new ColorHSV((int) (227.0 / 360.0 * 255.0), 255, 255);
     private Mat mRgba; //RGBA scene image
     private Mat mGray; //Grayscale scene image
     private CameraBridgeViewBase mOpenCvCameraView;
-
     private float focalLength; //Camera lens focal length
-
     //private ObjectDetection.ObjectAnalysis objectAnalysis;
     private FPS fpsCounter;
+    private final BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    // OpenCV loaded successfully!
+                    // Load native library AFTER OpenCV initialization
 
+                    initialize();
+
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+    private ColorBlobDetector detectorRed;
+    private ColorBlobDetector detectorBlue;
     public CameraTestActivity() {
 
     }
 
-    private void initialize()
-    {
+    private void initialize() {
         //GET CAMERA PROPERTIES
         Camera cam = Cameras.PRIMARY.createCamera();
         android.hardware.Camera.Parameters pam = cam.getCamera().getParameters();
@@ -79,28 +101,9 @@ public class CameraTestActivity extends Activity implements CvCameraViewListener
         fpsCounter = new FPS();
     }
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    // OpenCV loaded successfully!
-                    // Load native library AFTER OpenCV initialization
-
-                    initialize();
-
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
-
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -113,16 +116,14 @@ public class CameraTestActivity extends Activity implements CvCameraViewListener
     }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             // Internal OpenCV library not found. Using OpenCV Manager for initialization
@@ -139,22 +140,12 @@ public class CameraTestActivity extends Activity implements CvCameraViewListener
             mOpenCvCameraView.disableView();
     }
 
-    private ColorBlobDetector detectorRed;
-    private ColorBlobDetector detectorBlue;
-    private static final ColorHSV colorRadius = new ColorHSV(50, 75, 127);
-
-    private static final ColorHSV lowerBoundRed = new ColorHSV( (int)(305         / 360.0 * 255.0), (int)(0.200 * 255.0), (int)(0.300 * 255.0));
-    private static final ColorHSV upperBoundRed = new ColorHSV( (int)((360.0+5.0) / 360.0 * 255.0), 255                 , 255);
-
-    private static final ColorHSV lowerBoundBlue = new ColorHSV((int)(170.0       / 360.0 * 255.0), (int)(0.200 * 255.0), (int)(0.750 * 255.0));
-    private static final ColorHSV upperBoundBlue = new ColorHSV((int)(227.0       / 360.0 * 255.0), 255                 , 255);
-
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
         mGray = new Mat(height, width, CvType.CV_8UC1);
 
         //Initialize all detectors here
-        detectorRed  = new ColorBlobDetector(lowerBoundRed, upperBoundRed);
+        detectorRed = new ColorBlobDetector(lowerBoundRed, upperBoundRed);
         detectorBlue = new ColorBlobDetector(lowerBoundBlue, upperBoundBlue);
     }
 
@@ -192,7 +183,7 @@ public class CameraTestActivity extends Activity implements CvCameraViewListener
 
 
             //DEBUG confidence output
-            Drawing.drawText(mRgba, "Confidence: " + colorAnalysis.getConfidenceString() ,
+            Drawing.drawText(mRgba, "Confidence: " + colorAnalysis.getConfidenceString(),
                     new Point(0, 50), 1.0f, new ColorGRAY(255));
 
             //Transform.enlarge(mRgba, originalSize, true);
@@ -200,9 +191,7 @@ public class CameraTestActivity extends Activity implements CvCameraViewListener
 
             Drawing.drawText(mRgba, colorAnalysis.getStateLeft().toString() + ", " + colorAnalysis.getStateRight().toString(),
                     new Point(0, 8), 1.0f, new ColorGRAY(255), Drawing.Anchor.BOTTOMLEFT);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Drawing.drawText(mRgba, "Analysis Error", new Point(0, 8), 1.0f, new ColorRGBA("#F44336"), Drawing.Anchor.BOTTOMLEFT);
             e.printStackTrace();
         }
